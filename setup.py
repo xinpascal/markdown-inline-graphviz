e5c554e4fa8f0e1c4fda5c09ea4152cf14c24ca1
Cesar Morel <cmorel@psa.com.ar>
2019-06-06 00:37:56 -0300

    Add credits in long description field.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import sys
 from setuptools import setup
 
-VERSION = '1.0.2'
+VERSION = '1.0.3'
 
 if sys.argv[-1] == 'publish':
     if os.system("pip freeze | grep wheel"):
@@ -26,17 +26,15 @@ setup(
     author="Cesar Morel",
     author_email="cesaremoreln@gmail.com",
     description="Render inline graphs with Markdown and Graphviz (python3 version)",
+    long_description="This is just a continuation of the great job of Steffen Prince (sprin/markdown-inline-graphviz) in order to get it work with pip3. If you use python 2, please use the original extension instead.",
     license="MIT",
     url="https://github.com/cesaremorel/markdown-inline-graphviz",
     classifiers=[
         'Development Status :: 5 - Production/Stable',
-
         'Intended Audience :: Developers',
         'Topic :: Documentation',
         'Topic :: Text Processing',
-
         'License :: OSI Approved :: MIT License',
-
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
